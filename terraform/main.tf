9a349edf559d1fa1cb1dd610869a0e0380a66b47
Greed <daniil.tsitavets@student.ehu.lt>
2025-04-23 13:37:15 +0300

    s3 module (#1)
    * feature: define backend.tf
    
    * feature: define s3 module
    
    * feature: define initial main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.4.0"
+}
+
+module "s3" {
+  source        = "./modules/s3"
+  # s3_bucket_name = "coursework-data-bucket"
+}
