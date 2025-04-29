a480d7485a474c63279031d96c7dd7e666eedfff
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-04-29 21:20:40 +0300

    refactor: add public_subnet_ids to output.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -5,7 +5,9 @@ output "vpc_id" {
 output "private_subnet_ids" {
   value = [for subnet in aws_subnet.private_coursework_subnet : subnet.id]
 }
-
+output "public_subnet_ids" {
+  value = [for subnet in aws_subnet.public_coursework_subnet : subnet.id]
+}
 output "db_subnet_group" {
   value = aws_db_subnet_group.db_subnet_group.name
 }
