11abebaf0ae8360b621920c61ffaa0e20e0ed7ba
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-04-30 22:22:07 +0300

    refactor: rename var

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -10,6 +10,6 @@ output "public_subnet_ids" {
   value = [for subnet in aws_subnet.public_coursework_subnet : subnet.id]
 }
   
-output "db_subnet_group" {
+output "db_subnet_group_name" {
   value = aws_db_subnet_group.db_subnet_group.name
 }
