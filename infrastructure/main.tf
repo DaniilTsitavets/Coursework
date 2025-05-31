a1263e68ff77c78a57d043cb50afda01cdfd5e31
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-01 01:16:17 +0300

    feature: add keys for scripts in s3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,6 +48,8 @@ module "lambda_to_oltp" {
     DB_USER     = module.rds.db_username
     DB_PASSWORD = module.rds.db_password
     S3_BUCKET   = module.s3.bucket_name
-    S3_KEY      = "etl-scripts/insert_data.sql"
+    ETL_KEY     = module.s3.s3_key_oltp_etl
+    CSV_KEY     = module.s3.s3_key_test_csv
+    INIT_TABLES_KEY = module.s3.s3_key_init_tables
   }
 }
