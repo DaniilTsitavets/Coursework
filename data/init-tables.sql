2a20e783d6cda15aeab3248f46c813fb4411b7be
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-01 01:12:04 +0300

    feature: add script for initing all tables instead of using 2 separate

--- data/init-tables.sql
+++ data/init-tables.sql
@@ -1,25 +1,38 @@
-DROP TABLE IF EXISTS invoice_line;
-DROP TABLE IF EXISTS invoice;
-DROP TABLE IF EXISTS track;
-DROP TABLE IF EXISTS album;
-DROP TABLE IF EXISTS artist;
-DROP TABLE IF EXISTS customer;
-DROP TABLE IF EXISTS genre;
-DROP TABLE IF EXISTS media_type;
+CREATE TABLE IF NOT EXISTS staging_data (
+    title VARCHAR(160),
+    artist_name VARCHAR(120),
+    first_name VARCHAR(40),
+    last_name VARCHAR(20),
+    address VARCHAR(70),
+    city VARCHAR(40),
+    country VARCHAR(40),
+    phone VARCHAR(24),
+    email VARCHAR(60),
+    genre_name VARCHAR(120),
+    invoice_date TIMESTAMP,
+    billing_address VARCHAR(70),
+    billing_city VARCHAR(40),
+    billing_country VARCHAR(40),
+    total NUMERIC(10,2),
+    track_name VARCHAR(200),
+    media_type_name VARCHAR(120),
+    unit_price NUMERIC(10,2),
+    quantity INT
+);
 
-CREATE TABLE artist (
+CREATE TABLE IF NOT EXISTS artist (
     artist_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(120)
 );
 
-CREATE TABLE album (
+CREATE TABLE IF NOT EXISTS album (
     album_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR(160) NOT NULL,
     artist_id INT NOT NULL,
     FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
 );
 
-CREATE TABLE customer (
+CREATE TABLE IF NOT EXISTS customer (
     customer_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name VARCHAR(40) NOT NULL,
     last_name VARCHAR(20) NOT NULL,
@@ -30,17 +43,17 @@ CREATE TABLE customer (
     email VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE genre (
+CREATE TABLE IF NOT EXISTS genre (
     genre_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(120)
 );
 
-CREATE TABLE media_type (
+CREATE TABLE IF NOT EXISTS media_type (
     media_type_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(120)
 );
 
-CREATE TABLE track (
+CREATE TABLE IF NOT EXISTS track (
     track_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
     album_id INT,
@@ -52,7 +65,7 @@ CREATE TABLE track (
     FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
 
-CREATE TABLE invoice (
+CREATE TABLE IF NOT EXISTS invoice (
     invoice_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     customer_id INT NOT NULL,
     invoice_date TIMESTAMP NOT NULL,
@@ -63,7 +76,7 @@ CREATE TABLE invoice (
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
 
-CREATE TABLE invoice_line (
+CREATE TABLE IF NOT EXISTS invoice_line (
     invoice_line_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     invoice_id INT NOT NULL,
     track_id INT NOT NULL,
