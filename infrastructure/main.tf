2a4b185fc0e14d67163354c4110df12486aecc8f
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-01 01:24:21 +0300

    refactor: uncomment main

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,27 +8,27 @@ terraform {
   required_version = ">= 1.4.0"
 }
 
-# module "s3" {
-#   source = "./modules/s3"
-# }
+module "s3" {
+  source = "./modules/s3"
+}
 
 module "vpc" {
   source = "./modules/vpc"
 }
-#
-# module "ec2-bastion" {
-#   source           = "./modules/ec2-bastion"
-#   vpc_id           = module.vpc.vpc_id
-#   public_subnet_id = module.vpc.public_subnet_ids[0]
-# }
-#
-# module "rds" {
-#   source               = "./modules/rds"
-#   vpc_id               = module.vpc.vpc_id
-#   db_subnet_group_name = module.vpc.db_subnet_group_name
-#   cidr_ipv4_ingress    = module.ec2-bastion.instance_private_ip
-#   lambda_sg            = module.lambda_to_oltp.lambda_sg
-# }
+
+module "ec2-bastion" {
+  source           = "./modules/ec2-bastion"
+  vpc_id           = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_ids[0]
+}
+
+module "rds" {
+  source               = "./modules/rds"
+  vpc_id               = module.vpc.vpc_id
+  db_subnet_group_name = module.vpc.db_subnet_group_name
+  cidr_ipv4_ingress    = module.ec2-bastion.instance_private_ip
+  lambda_sg            = module.lambda_to_oltp.lambda_sg
+}
 
 module "lambda_to_oltp" {
   source             = "./modules/lambda"
@@ -42,14 +42,14 @@ module "lambda_to_oltp" {
     "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
   ]
 
-  # env_variables = {
-  #   DB_HOST     = module.rds.db_address
-  #   DB_NAME     = module.rds.db_name
-  #   DB_USER     = module.rds.db_username
-  #   DB_PASSWORD = module.rds.db_password
-  #   S3_BUCKET   = module.s3.bucket_name
-  #   ETL_KEY     = module.s3.s3_key_oltp_etl
-  #   CSV_KEY     = module.s3.s3_key_test_csv
-  #   INIT_TABLES_KEY = module.s3.s3_key_init_tables
-  # }
+  env_variables = {
+    DB_HOST     = module.rds.db_address
+    DB_NAME     = module.rds.db_name
+    DB_USER     = module.rds.db_username
+    DB_PASSWORD = module.rds.db_password
+    S3_BUCKET   = module.s3.bucket_name
+    ETL_KEY     = module.s3.s3_key_oltp_etl
+    CSV_KEY     = module.s3.s3_key_test_csv
+    INIT_TABLES_KEY = module.s3.s3_key_init_tables
+  }
 }
