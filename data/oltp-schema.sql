0a9b831560d79007b8b69436d456f13f20c1b403
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-05-31 18:39:48 +0300

    feature: add schema for main tables

---
+++ data/oltp-schema.sql
@@ -0,0 +1,74 @@
+DROP TABLE IF EXISTS invoice_line;
+DROP TABLE IF EXISTS invoice;
+DROP TABLE IF EXISTS track;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS media_type;
+
+CREATE TABLE artist (
+    artist_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(120)
+);
+
+CREATE TABLE album (
+    album_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(160) NOT NULL,
+    artist_id INT NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
+);
+
+CREATE TABLE customer (
+    customer_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    address VARCHAR(70),
+    city VARCHAR(40),
+    country VARCHAR(40),
+    phone VARCHAR(24),
+    email VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE genre (
+    genre_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(120)
+);
+
+CREATE TABLE media_type (
+    media_type_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(120)
+);
+
+CREATE TABLE track (
+    track_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    album_id INT,
+    media_type_id INT NOT NULL,
+    genre_id INT,
+    unit_price NUMERIC(10,2) NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES album(album_id),
+    FOREIGN KEY (media_type_id) REFERENCES media_type(media_type_id),
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+);
+
+CREATE TABLE invoice (
+    invoice_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    customer_id INT NOT NULL,
+    invoice_date TIMESTAMP NOT NULL,
+    billing_address VARCHAR(70),
+    billing_city VARCHAR(40),
+    billing_country VARCHAR(40),
+    total NUMERIC(10,2) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+);
+
+CREATE TABLE invoice_line (
+    invoice_line_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    invoice_id INT NOT NULL,
+    track_id INT NOT NULL,
+    unit_price NUMERIC(10,2) NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (invoice_id) REFERENCES invoice(invoice_id),
+    FOREIGN KEY (track_id) REFERENCES track(track_id)
+);
