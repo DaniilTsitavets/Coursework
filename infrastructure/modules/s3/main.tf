0fd1b688c96279abfdcb2da3baaa7a0d2a67b4da
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-01 01:15:32 +0300

    feature: push objects(elt, test-csv, init-tables) to s3 bucket

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -9,3 +9,24 @@ resource "aws_s3_bucket" "s3-coursework-bucket" {
   bucket = var.s3_bucket_name != null ? var.s3_bucket_name : "coursework-bucket-${random_string.suffix.result}"
 
 }
+
+resource "aws_s3_object" "etl1" {
+  bucket       = aws_s3_bucket.s3-coursework-bucket.bucket
+  key          = "etl1/from-staging-to-main.sql"
+  source       = "../data/from-staging-to-main.sql"
+  etag = filemd5("../data/from-staging-to-main.sql")
+}
+
+resource "aws_s3_object" "test-csv" {
+  bucket       = aws_s3_bucket.s3-coursework-bucket.bucket
+  key          = "data/test.csv"
+  source       = "../data/test.csv"
+  etag = filemd5("../data/test.csv")
+}
+
+resource "aws_s3_object" "init-tables" {
+  bucket       = aws_s3_bucket.s3-coursework-bucket.bucket
+  key          = "init/init-tables.sql"
+  source       = "../data/init-tables.sql"
+  etag = filemd5("../data/init-tables.sql")
+}
