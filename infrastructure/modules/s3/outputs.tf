84541b86ae25630c242c2a1894902cb0f887dc4e
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-01 01:17:44 +0300

    refactor: provide outputs for s3 keys

--- infrastructure/modules/s3/outputs.tf
+++ infrastructure/modules/s3/outputs.tf
@@ -2,3 +2,15 @@ output "bucket_name" {
   description = "Name of the created S3 bucket"
   value       = aws_s3_bucket.s3-coursework-bucket.bucket
 }
+
+output "s3_key_oltp_etl" {
+  value = aws_s3_object.etl1.key
+}
+
+output "s3_key_test_csv" {
+  value = aws_s3_object.test-csv.key
+}
+
+output "s3_key_init_tables" {
+  value = aws_s3_object.init-tables.key
+}
