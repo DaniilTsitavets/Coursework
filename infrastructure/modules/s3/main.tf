850f442162fa5cfba076c3a1206aca1fa9e87fac
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-04 19:24:20 +0300

    refactor: match resources to new paths

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -9,11 +9,12 @@ resource "aws_s3_bucket" "s3-coursework-bucket" {
   bucket = var.s3_bucket_name != null ? var.s3_bucket_name : "coursework-bucket-${random_string.suffix.result}"
 }
 
+//TODO create vars for paths
 resource "aws_s3_object" "etl1" {
   bucket       = aws_s3_bucket.s3-coursework-bucket.bucket
   key          = "etl1/from-staging-to-main.sql"
-  source       = "../data/from-staging-to-main.sql"
-  etag = filemd5("../data/from-staging-to-main.sql")
+  source       = "../sql/from-staging-to-main.sql"
+  etag = filemd5("../sql/from-staging-to-main.sql")
 }
 
 resource "aws_s3_object" "test-csv" {
@@ -26,6 +27,6 @@ resource "aws_s3_object" "test-csv" {
 resource "aws_s3_object" "init-tables" {
   bucket       = aws_s3_bucket.s3-coursework-bucket.bucket
   key          = "init/init-tables.sql"
-  source       = "../data/init-tables.sql"
-  etag = filemd5("../data/init-tables.sql")
+  source       = "../sql/init-tables.sql"
+  etag = filemd5("../sql/init-tables.sql")
 }
