36c6887ce75df0aae183d6fb1698fc02eea668aa
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-04 19:15:43 +0300

    refactor: match naming for global outputs

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -4,5 +4,5 @@ output "bucket_name" {
 
 output "connect_to_db" {
   description = "Shows ssh command to db"
-  value = "ssh -i ./Downloads/pem/coursework-bastion-kp.pem -L 5432:${module.rds.db_endpoint} ubuntu@${module.ec2-bastion.ssh_connect_ip}"
+  value = "ssh -i ./Downloads/pem/coursework-bastion-kp.pem -L 5432:${module.rds_oltp.db_endpoint} ubuntu@${module.ec2-bastion.ssh_connect_ip}"
 }
