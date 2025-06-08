07d4b7f000a4ffde2efab2d76dcb05634268b030
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-08 15:16:35 +0300

    refactor: create lambda_to_olap resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,7 +27,7 @@ module "rds_oltp" {
   vpc_id               = module.vpc.vpc_id
   db_subnet_group_name = module.vpc.db_subnet_group_name
   bastion_sg           = module.ec2-bastion.bastion_sg
-  lambda_sg_ids        = [module.lambda_to_oltp.lambda_sg] //TODO do not forget to add lambda_to_olap id
+  lambda_sg_ids = [module.lambda_to_oltp.lambda_sg, module.lambda_to_olap.lambda_sg]
 }
 
 module "lambda_to_oltp" {
@@ -50,15 +50,44 @@ module "lambda_to_oltp" {
     S3_BUCKET       = module.s3.bucket_name
     ETL_KEY         = module.s3.s3_key_oltp_etl
     CSV_KEY         = module.s3.s3_key_test_csv
-    INIT_TABLES_KEY = module.s3.s3_key_init_tables
+    INIT_TABLES_KEY = module.s3.s3_key_init_oltp_tables
   }
 }
 
 module "rds_olap" {
-  source = "./modules/rds"
-  db_name = "OLAPdb"
+  source               = "./modules/rds"
+  db_name              = "OLAPdb"
   vpc_id               = module.vpc.vpc_id
   db_subnet_group_name = module.vpc.db_subnet_group_name
   bastion_sg           = module.ec2-bastion.bastion_sg
-  lambda_sg_ids        = [] //TODO do not forget to add lambda_to_olap id
+  lambda_sg_ids = [module.lambda_to_olap.lambda_sg]
+}
+
+module "lambda_to_olap" {
+  source             = "./modules/lambda"
+  filename           = "../build/lambda-to-olapp.zip"
+  function_name      = "lambda-to-olap"
+  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id             = module.vpc.vpc_id
+  policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  ]
+
+  env_variables = {
+    OLTP_HOST     = module.rds_oltp.db_address
+    OLTP_NAME     = module.rds_oltp.db_name
+    OLTP_USER     = module.rds_oltp.db_username
+    OLTP_PASSWORD = module.rds_oltp.db_password
+
+    OLAP_HOST     = module.rds_olap.db_address
+    OLAP_NAME     = module.rds_olap.db_name
+    OLAP_USER     = module.rds_olap.db_username
+    OLAP_PASSWORD = module.rds_olap.db_password
+
+    INIT_SQL_KEY = module.s3.s3_key_init_olap_tables
+    ETL_SQL_KEY  = module.s3.s3_key_olap_etl
+    S3_BUCKET    = module.s3.bucket_name
+  }
 }
