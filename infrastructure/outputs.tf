640dd3b27a29687465d14c22f58dd609e5a9550b
Daniil Tsitavets <d.tsitavets@gmail.com>
2025-06-13 23:06:35 +0300

    feature: output for ssh tunnel

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,7 +2,12 @@ output "bucket_name" {
   value = module.s3.bucket_name
 }
 
-output "connect_to_db" {
+output "connect_to_dbs" {
   description = "Shows ssh command to db"
-  value = "ssh -i ./Downloads/pem/coursework-bastion-kp.pem -L 5432:${module.rds_oltp.db_endpoint} ubuntu@${module.ec2-bastion.ssh_connect_ip}"
+  value = <<EOT
+ssh -i ./Downloads/pem/coursework-bastion-kp.pem \
+  -L 5432:${module.rds_oltp.db_endpoint} \
+  -L 5433:${module.rds_olap.db_endpoint} \
+  ubuntu@${module.ec2-bastion.ssh_connect_ip}
+EOT
 }
